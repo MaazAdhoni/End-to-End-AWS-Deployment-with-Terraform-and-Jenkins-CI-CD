59ebc5c13ae3b596acc141b59e517a6a8ee6797b
Maaz Adhoni <maazadhoni@gmail.com>
2024-08-03 17:51:27 +0530

    These are the improvement - 3 in the code

--- s3.tf
+++ s3.tf
@@ -4,12 +4,6 @@ resource "aws_s3_bucket" "my-s3-bucket" {
   tags = var.tags
 }
 
-resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  
-  tags = var.tags
-}
-
 resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
   bucket = aws_s3_bucket.my-s3-bucket.id
   versioning_configuration {
