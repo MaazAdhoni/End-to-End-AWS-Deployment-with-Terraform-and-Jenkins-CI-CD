49c001c4d97a9be7107dd4a2f905ebd639cc216e
Maaz Adhoni <maazadhoni@gmail.com>
2024-08-03 17:35:42 +0530

    These are the improvement - 2 in the code

--- s3.tf
+++ s3.tf
@@ -1,14 +1,16 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
+  
   tags = var.tags
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.my-s3-bucket.bucket
-  acl    = "private"
+resource "aws_s3_bucket" "my-s3-bucket" {
+  bucket_prefix = var.bucket_prefix
+  
+  tags = var.tags
 }
 
-resource "aws_s3_bucket_versioning" "example" {
+resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
   bucket = aws_s3_bucket.my-s3-bucket.id
   versioning_configuration {
     status = var.versioning ? "Enabled" : "Suspended"
